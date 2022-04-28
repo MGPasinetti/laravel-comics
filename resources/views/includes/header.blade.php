305c98c82090a5fccb3619ec7925c95e9966cf95
MGPasinetti <mg.pasinetti@gmail.com>
2022-04-28 08:59:33 +0200

    header done

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -19,21 +19,22 @@
 
             <nav>
                 <ul>
-                    <li><a href="{{ route('characters') }}">CHARACTERS</a></li>
-                    <li><a href="{{ route('comics') }}">COMICS</a></li>
-                    <li><a href="{{ route('movies') }}">MOVIES</a></li>
-                    <li><a href="{{ route('TV') }}">TV</a></li>
-                    <li><a href="{{ route('games') }}">GAMES</a></li>
-                    <li><a href="{{ route('collectibles') }}">COLLECTIBLES</a></li>
-                    <li><a href="{{ route('videos') }}">VIDEOS</a></li>
-                    <li><a href="{{ route('fans') }}">FANS</a></li>
-                    <li><a href="{{ route('news') }}">NEWS</a></li>
-                    <li><a href="{{ route('shop') }}">SHOP <i class="fa-solid fa-caret-down" style="color: #0282F9"></i></a></li>
+                    <li class="{{ (Route::current()->getName() == 'characters') ? 'active' : '' }}"><a href="{{ route('characters') }}">CHARACTERS</a></li>
+                    <li class="{{ (Route::current()->getName() == 'comics') ? 'active' : '' }}"><a href="{{ route('comics') }}">COMICS</a></li>
+                    <li class="{{ (Route::current()->getName() == 'movies') ? 'active' : '' }}"><a href="{{ route('movies') }}">MOVIES</a></li>
+                    <li class="{{ (Route::current()->getName() == 'TV') ? 'active' : '' }}"><a href="{{ route('TV') }}">TV</a></li>
+                    <li class="{{ (Route::current()->getName() == 'games') ? 'active' : '' }}"><a href="{{ route('games') }}">GAMES</a></li>
+                    <li class="{{ (Route::current()->getName() == 'collectibles') ? 'active' : '' }}"><a href="{{ route('collectibles') }}">COLLECTIBLES</a></li>
+                    <li class="{{ (Route::current()->getName() == 'videos') ? 'active' : '' }}"><a href="{{ route('videos') }}">VIDEOS</a></li>
+                    <li class="{{ (Route::current()->getName() == 'fans') ? 'active' : '' }}"><a href="{{ route('fans') }}">FANS</a></li>
+                    <li class="{{ (Route::current()->getName() == 'news') ? 'active' : '' }}"><a href="{{ route('news') }}">NEWS</a></li>
+                    <li class="{{ (Route::current()->getName() == 'shop') ? 'active' : '' }}"><a href="{{ route('shop') }}">SHOP <i class="fa-solid fa-caret-down" style="color: #0282F9"></i></a></li>
                 </ul>
             </nav>
 
             <div class="searchbar">
                 <input type="text" name="search" id="searchbar" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
     </div>
